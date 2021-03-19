6c8641f2f03a7fd12523bf93c0338439e8fc84cb
Terry Wong <terrywon@cisco.com>
2021-03-19 13:53:40 -0700

    Saved code

--- backend/chalicelib/data_layers/s3.py
+++ backend/chalicelib/data_layers/s3.py
@@ -1,5 +1,7 @@
 import logging
 
+from botocore.exceptions import ClientError
+
 from .. import aws_session, s3_file_bucket_name
 from ..constants import APP_NAME
 
@@ -10,12 +12,64 @@ s3 = aws_session.client("s3")
 
 def s3_upload_file(file, file_path):
     logger.info("Uploading a file to S3.")
-    response = s3.put_object(Body=file, Bucket=s3_file_bucket_name, Key=file_path)
-    logger.debug(f"S3 put_object() response: {response}")
+    try:
+        response = s3.put_object(Body=file, Bucket=s3_file_bucket_name, Key=file_path)
+        logger.debug(f"S3 put_object() response: {response}")
+    except ClientError as e:
+        logging.error(e)
+        raise
 
 
 def s3_download_file(file_path):
-    response = s3.get_object(Bucket=s3_file_bucket_name, Key=file_path)
-    logger.debug(f"S3 get_object() response: {response}")
+    try:
+        response = s3.get_object(Bucket=s3_file_bucket_name, Key=file_path)
+        logger.debug(f"S3 get_object() response: {response}")
 
-    return response.get("Body", b"").read()
+        return response.get("Body", b"").read()
+    except ClientError as e:
+        logging.error(e)
+        raise
+
+
+def s3_get_upload_url(file_path, content_type):
+    logger.info("Generating a pre-signed upload URL for a file in S3.")
+    try:
+        params = {
+            "Bucket": s3_file_bucket_name,
+            "Key": file_path,
+        }
+        if content_type:
+            params["ContentType"] = content_type
+        presigned_url = s3.generate_presigned_url(
+            ClientMethod="put_object",
+            Params=params,
+            ExpiresIn=604800,
+        )
+        logger.debug(f"S3 generate_presigned_url() response: {presigned_url}")
+
+        return presigned_url
+    except ClientError as e:
+        logging.error(e)
+        raise
+
+
+def s3_get_download_url(file_path, content_type):
+    logger.info("Generating a pre-signed download URL for a file in S3.")
+    try:
+        params = {
+            "Bucket": s3_file_bucket_name,
+            "Key": file_path,
+        }
+        if content_type:
+            params["ContentType"] = content_type
+        presigned_url = s3.generate_presigned_url(
+            ClientMethod="get_object",
+            Params=params,
+            ExpiresIn=604800,
+        )
+        logger.debug(f"S3 generate_presigned_url() response: {presigned_url}")
+
+        return presigned_url
+    except ClientError as e:
+        logging.error(e)
+        raise
