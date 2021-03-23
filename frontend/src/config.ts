024d78747767b3c79c8c1b284c911f8871d338d4
Terry Wong <terrywon@cisco.com>
2021-03-23 10:55:08 -0700

    Updated callback

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -6,5 +6,6 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'kungfulucky7.us.auth0.com',            // Auth0 domain
   clientId: 'sSGU8JrhHWRG6poOIDEDjcCiy4XXY0q6',          // Auth0 client id
-  callbackUrl: 'http://localhost/callback'
+  // callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'http://file-upload-frontend-dev.us-east-1.elasticbeanstalk.com/callback'
 }
